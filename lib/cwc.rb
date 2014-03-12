63c74544cce2a18516118f65631531517aa89bcb
Hans Gamarra <hans@appmeup.co>
2014-03-12 18:25:39 -0500

    Testing

--- lib/cwc.rb
+++ lib/cwc.rb
@@ -3,22 +3,5 @@ class Cwc
     puts "Hello world!"
   end
 
-  def request_url(url)
-    uri = URI.parse(url)
-
-    client = Net::HTTP.new(uri.host, uri.port)
-
-    if uri.scheme == "https"
-      client.use_ssl = true
-      if !Redzirra.options[:ca_file].nil?
-        client.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        client.ca_file = Redzirra.options[:ca_file]
-      else
-        client.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-    end
-
-    client.read_timeout = 5*60 #let's wait 5minutes before a timeout.
-    client.request_get(uri.request_uri)
-  end
+  
 end
