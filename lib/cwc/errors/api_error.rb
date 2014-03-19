c9973ce5f799165aab4938ce930ecb4cd41560ab
Hans Gamarra <hans@appmeup.co>
2014-03-19 17:45:46 -0500

    Fixed to_s in API error

--- lib/cwc/errors/api_error.rb
+++ lib/cwc/errors/api_error.rb
@@ -4,22 +4,23 @@ module Cwc
   class ApiError < CwcError
     include Cwc::Utils::XML
 
-    def initialize message=nil, response=nil
+    def initialize(message=nil, response=nil)
       super(message)
       @response = response unless response.nil?
     end
 
     def to_s
-      ANSI.red(@message)
+      result = super()
       # Parse errors
       if @response
         errors = parse_errors @response
         if errors.size > 0
-          puts ANSI.red(errors.join("\n"))
-          puts ANSI.red("Body of the response:")
-          puts ANSI.red(@response.body)
+          result += ANSI.red(errors.join("\n"))
+          result += ANSI.red("Body of the response:")
+          result += ANSI.red(@response.body)
         end
       end
+      result
     end
   end
 end
