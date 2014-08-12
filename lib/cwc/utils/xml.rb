55b36f043ed588eba20e6e971e1647f467608459
Hans Gamarra <hans@appmeup.co>
2014-08-11 19:27:44 -0500

    Fixed XML structure assembly

--- lib/cwc/utils/xml.rb
+++ lib/cwc/utils/xml.rb
@@ -13,66 +13,73 @@ module Cwc
       #Parse data from @data as XML according to standard 2.0
       def parse_xml data={}
         # XML data
+        xml = ""
         begin
-          %(<?xml version="1.0" ?>
-            <CWC>
-              <CWCVersion>#{Cwc.api_version_number}</CWCVersion>
-              <Delivery>
-                <DeliveryId>#{data[:delivery][:id]}</DeliveryId>
-                <DeliveryDate>#{data[:delivery][:date]}</DeliveryDate>
-                <DeliveryAgent>#{data[:delivery][:agent]}</DeliveryAgent>
-                <DeliveryAgentAckEmailAddress>#{data[:delivery][:ackemailaddress]}</DeliveryAgentAckEmailAddress>
-                <DeliveryAgentContact>
-                  <DeliveryAgentContactName>#{data[:delivery][:agentcontact][:name]}</DeliveryAgentContactName>
-                  <DeliveryAgentContactEmail>#{data[:delivery][:agentcontact][:email]}</DeliveryAgentContactEmail>
-                  <DeliveryAgentContactPhone>#{data[:delivery][:agentcontact][:phone]}</DeliveryAgentContactPhone>
-                </DeliveryAgentContact>
-                <Organization>#{data[:delivery][:organization]}</Organization>
-                <OrganizationContact>
-                  <OrganizationContactName>#{data[:delivery][:organizationcontact][:name]}</OrganizationContactName>
-                  <OrganizationContactEmail>#{data[:delivery][:organizationcontact][:email]}</OrganizationContactEmail>
-                  <OrganizationContactPhone>#{data[:delivery][:organizationcontact][:phone]}</OrganizationContactPhone>
-                </OrganizationContact>
-                <OrganizationAbout>#{data[:delivery][:organizationabout]}</OrganizationAbout>
-                <CampaignId>#{data[:delivery][:campaignid]}</CampaignId>
-              </Delivery>
-              <Recipient>
-                <MemberOffice>#{data[:recipient][:memberoffice]}</MemberOffice>
-                <IsResponseRequested>#{data[:recipient][:isresponserequested]}</IsResponseRequested>
-                <NewsletterOptIn>#{data[:recipient][:newsletteroptin]}</NewsletterOptIn>
-              </Recipient>
-              <Constituent>
-                <Prefix>#{data[:constituent][:prefix]}</Prefix>
-                <FirstName>#{data[:constituent][:firstname]}</FirstName>
-                <MiddleName>#{data[:constituent][:middlename]}</MiddleName>
-                <LastName>#{data[:constituent][:lastname]}</LastName>
-                <Suffix>#{data[:constituent][:suffix]}</Suffix>
-                <Title>#{data[:constituent][:title]}</Title>
-                <ConstituentOrganization>#{data[:constituent][:constituentorganization]}</ConstituentOrganization>
-                <Address1>#{data[:constituent][:address1]}</Address1>
-                <Address2>#{data[:constituent][:address2]}</Address2>
-                <City>#{data[:constituent][:city]}</City>
-                <StateAbbreviation>#{data[:constituent][:stateabbreviation]}</StateAbbreviation>
-                <Zip>#{data[:constituent][:zip]}</Zip>
-                <Phone>#{data[:constituent][:phone]}</Phone>
-                <AddressValidation>#{data[:constituent][:addressvalidation]}</AddressValidation>
-                <Email>#{data[:constituent][:email]}</Email>
-                <EmailValidation>#{data[:constituent][:emailvalidation]}</EmailValidation>
-              </Constituent>
-              <Message>
-                <Subject>#{data[:message][:subject]}</Subject>
-                <LibraryOfCongressTopics>
-                  #{ get_topics_from_data data }
-                </LibraryOfCongressTopics>
-                <Bills>
-                  #{ get_bills_from_data data }
-                </Bills>
-                <ProOrCon>#{data[:message][:proorcon]}</ProOrCon>
-                <OrganizationStatement>#{data[:message][:organizationstatement]}</OrganizationStatement>
-                <ConstituentMessage>#{data[:message][:constituentmessage]}</ConstituentMessage>
-                <MoreInfo>#{data[:message][:moreinfo]}</MoreInfo>
-              </Message>
-            </CWC>)
+          xml += '<?xml version="1.0" ?>'
+          xml += "<CWC>"
+          xml += "<CWCVersion>#{Cwc.api_version_number}</CWCVersion>"
+          xml += "<Delivery>"
+          xml += "<DeliveryId>#{data[:delivery][:id]}</DeliveryId>"
+          xml += "<DeliveryDate>#{data[:delivery][:date]}</DeliveryDate>"
+          xml += "<DeliveryAgent>#{data[:delivery][:agent]}</DeliveryAgent>"
+          xml += "<DeliveryAgentAckEmailAddress>#{data[:delivery][:ackemailaddress]}</DeliveryAgentAckEmailAddress>"
+          xml += "<DeliveryAgentContact>"
+          xml += "<DeliveryAgentContactName>#{data[:delivery][:agentcontact][:name]}</DeliveryAgentContactName>"
+          xml += "<DeliveryAgentContactEmail>#{data[:delivery][:agentcontact][:email]}</DeliveryAgentContactEmail>"
+          xml += "<DeliveryAgentContactPhone>#{data[:delivery][:agentcontact][:phone]}</DeliveryAgentContactPhone>"
+          xml += "</DeliveryAgentContact>"
+          if data[:delivery][:organization]
+            xml += "<Organization>#{data[:delivery][:organization]}</Organization>"
+          end
+          if data[:delivery][:organizationcontact]
+            xml += "<OrganizationContact>"
+            xml += "<OrganizationContactName>#{data[:delivery][:organizationcontact][:name]}</OrganizationContactName>"
+            xml += "<OrganizationContactEmail>#{data[:delivery][:organizationcontact][:email]}</OrganizationContactEmail>"
+            xml += "<OrganizationContactPhone>#{data[:delivery][:organizationcontact][:phone]}</OrganizationContactPhone>"
+            xml += "</OrganizationContact>"
+          end
+          xml += "<OrganizationAbout>#{data[:delivery][:organizationabout]}</OrganizationAbout>" if data[:delivery][:organizationabout]
+          xml += "<CampaignId>#{data[:delivery][:campaignid]}</CampaignId>"
+          xml += "</Delivery>"
+          xml += "<Recipient>"
+          xml += "<MemberOffice>#{data[:recipient][:memberoffice]}</MemberOffice>"
+          xml += "<IsResponseRequested>#{data[:recipient][:isresponserequested]}</IsResponseRequested>" if data[:recipient][:isresponserequested]
+          xml += "<NewsletterOptIn>#{data[:recipient][:newsletteroptin]}</NewsletterOptIn>" if data[:recipient][:newsletteroptin]
+          xml += "</Recipient>"
+          xml += "<Constituent>"
+          xml += "<Prefix>#{data[:constituent][:prefix]}</Prefix>"
+          xml += "<FirstName>#{data[:constituent][:firstname]}</FirstName>"
+          xml += "<MiddleName>#{data[:constituent][:middlename]}</MiddleName>" if data[:constituent][:middlename]
+          xml += "<LastName>#{data[:constituent][:lastname]}</LastName>"
+          xml += "<Suffix>#{data[:constituent][:suffix]}</Suffix>" if data[:constituent][:suffix]
+          xml += "<Title>#{data[:constituent][:title]}</Title>" if data[:constituent][:title]
+          xml += "<ConstituentOrganization>#{data[:constituent][:constituentorganization]}</ConstituentOrganization>" if data[:constituent][:constituentorganization]
+          xml += "<Address1>#{data[:constituent][:address1]}</Address1>"
+          xml += "<Address2>#{data[:constituent][:address2]}</Address2>" if data[:constituent][:address2]
+          xml += "<City>#{data[:constituent][:city]}</City>"
+          xml += "<StateAbbreviation>#{data[:constituent][:stateabbreviation]}</StateAbbreviation>"
+          xml += "<Zip>#{data[:constituent][:zip]}</Zip>"
+          xml += "<Phone>#{data[:constituent][:phone]}</Phone>" if data[:constituent][:phone]
+          xml += "<AddressValidation>#{data[:constituent][:addressvalidation]}</AddressValidation>" if data[:constituent][:addressvalidation]
+          xml += "<Email>#{data[:constituent][:email]}</Email>"
+          xml += "<EmailValidation>#{data[:constituent][:emailvalidation]}</EmailValidation>" if data[:constituent][:emailvalidation]
+          xml += "</Constituent>"
+          xml += "<Message>"
+          xml += "<Subject>#{data[:message][:subject]}</Subject>"
+          xml += "<LibraryOfCongressTopics>"
+          xml += "#{ get_topics_from_data data }"
+          xml += "</LibraryOfCongressTopics>"
+          if data[:message][:bills]
+            xml += "<Bills>"
+            xml += "#{ get_bills_from_data data }"
+            xml += "</Bills>"
+          end
+          xml += "<ProOrCon>#{data[:message][:proorcon]}</ProOrCon>" if data[:message][:proorcon]
+          xml += "<OrganizationStatement>#{data[:message][:organizationstatement]}</OrganizationStatement>" if data[:message][:organizationstatement]
+          xml += "<ConstituentMessage>#{data[:message][:constituentmessage]}</ConstituentMessage>" if data[:message][:constituentmessage]
+          xml += "<MoreInfo>#{data[:message][:moreinfo]}</MoreInfo>" if data[:message][:moreinfo]
+          xml += "</Message>"
+          xml += "</CWC>"
           rescue StandardError => e
             # Get the line in the code where the error was generated
             error_line = e.backtrace[0].split(":")[1].to_i
@@ -84,6 +91,7 @@ module Cwc
               raise XMLSyntaxError.new("Error in XML in line: #{error_line.to_s}")
             end
           end
+          xml
       end
 
       def get_topics_from_data data
--- lib/cwc/version.rb
+++ lib/cwc/version.rb
@@ -1,3 +1,3 @@
 module Cwc
-  VERSION = "0.1.8"
+  VERSION = "0.1.9"
 end
